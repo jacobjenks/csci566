88a617466dee058f547feb3fa13b612598953049
Jacob Jenks <jacobjenks@gmail.com>
2015-04-21 16:58:18 -0600

    SQL syntax fixes

--- db/food_db.sql
+++ db/food_db.sql
@@ -1,25 +1,24 @@
 --	Table for holding image ids and image urls
 --
 CREATE TABLE IF NOT EXISTS image(
-	id int AUTOINCREMENT,
-	url varchar(512) NOT NULL,
-	PRIMARY KEY(id)
+	id integer PRIMARY KEY,
+	url varchar(512) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS hit(
 	image_id int NOT NULL,
-	task_tier varchar(10) NOT NULL
+	task_tier varchar(10) NOT NULL,
 	hit_id varchar(128) NOT NULL,
 	PRIMARY KEY(image_id, hit_id)
 	FOREIGN KEY(image_id)
-		REFERENCES image(id)
+	REFERENCES image(id)
 );
 
 CREATE TABLE IF NOT EXISTS food(
-	image_id int NOT NULL
+	image_id int NOT NULL,
 	task_tier varchar(10) NOT NULL,
 	quantity float NOT NULL,
 	PRIMARY KEY(image_id, task_tier),
 	FOREIGN KEY(image_id)
-		REFERENCES image(id)
+	REFERENCES image(id)
 );
