dcc47cbf95f2637a7d712f292789e832fa0f0c59
Jacob Jenks <jacobjenks@gmail.com>
2015-04-21 16:53:29 -0600

    Added table for storing results

--- db/food_db.sql
+++ db/food_db.sql
@@ -1,16 +1,25 @@
 --	Table for holding image ids and image urls
 --
 CREATE TABLE IF NOT EXISTS image(
-	id integer AUTOINCREMENT,
+	id int AUTOINCREMENT,
 	url varchar(512) NOT NULL,
 	PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS hit(
 	image_id int NOT NULL,
-	task_tier int NOT NULL,
+	task_tier varchar(10) NOT NULL
 	hit_id varchar(128) NOT NULL,
 	PRIMARY KEY(image_id, hit_id)
+	FOREIGN KEY(image_id)
+		REFERENCES image(id)
+);
+
+CREATE TABLE IF NOT EXISTS food(
+	image_id int NOT NULL
+	task_tier varchar(10) NOT NULL,
+	quantity float NOT NULL,
+	PRIMARY KEY(image_id, task_tier),
 	FOREIGN KEY(image_id)
 		REFERENCES image(id)
 );
