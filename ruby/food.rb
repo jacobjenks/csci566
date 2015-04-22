e521d0ab1f823d3951870c32c23bc62259bf1358
Jacob Jenks <jacobjenks@gmail.com>
2015-04-21 20:07:10 -0600

    The whole shebang is almost done. Just need to do the quantity questions now

--- ruby/food.rb
+++ ruby/food.rb
@@ -5,13 +5,17 @@ require 'bundler/setup'
 require 'mturk'
 require 'sqlite3'
 
+#################### Variables ##########################
 @mturk = Amazon::WebServices::MechanicalTurkRequester.new :Host => :Sandbox
 
 # Use this line instead if you want the production website.
 #@mturk = Amazon::WebServices::MechanicalTurkRequester.new :Host => :Production
 
-@base_directory = File.join(File.expand_path(File.dirname(__FILE__)), "../")
-@db = SQLite3::Database.open base_directory+"db/food_dev.db"
+@base_directory = File.expand_path(File.dirname(__FILE__))
+@base_directory = @base_directory.gsub(/ruby/, "")
+@db = SQLite3::Database.open @base_directory+"db/food_dev.db"
+
+#################### Functions ###########################
 
 #Make sure answers make sense
 #Response must have at least one answer, and no conflicting answers.
@@ -23,8 +27,8 @@ def validateAnswer(answers)
 		return false
 	end
 	
-	answers.each do |answer|
-		if(/Q|q/ =~ answer && answers.length > 1)#Invalid response, since this indicates them rejecting category and then choosing from it
+	answers.each do |key, answer|
+		if(/Q|q/ =~ answer.to_s && answers.length > 1)#Invalid response, since this indicates them rejecting category and then choosing from it
 			result = false
 		end
 	end
@@ -37,15 +41,19 @@ def processReviewableHits
 	puts "--Processing reviewable HITs--"
 	approved = 0
 	reject = 0
-	#Get reviewable hits
-	reviewable = @mturk.getReviewableHITs()
+	
+	reviewable = @db.execute("SELECT hit_id FROM hit WHERE complete!=1")
 
 	#Get assignments awaiting approval, and validate
-	reviewable[:HIT].each do |hit|
+	reviewable.each do |hit|
 		#get responses
-		answers = @mturk.getAssignmentsForHIT(:HITId => hit[:HITId])
-		
+		answers = @mturk.getAssignmentsForHIT(:HITId => hit)
 		if(answers[:NumResults] > 0)
+			if(answers[:NumResults] == 1)#fix inconsistent formatting
+				puts answers
+				puts "HEY YOU FORGOT TO FIX THIS"
+				exit
+			end
 			#validate each response in hit
 			answers[:Assignment].each do |assign|
 				if(assign[:AssignmentStatus]=='Submitted')
@@ -60,37 +68,56 @@ def processReviewableHits
 			end
 		end
 	end
+	
 	puts "     Approved: " + approved.to_s
 	puts "     Rejected: " + reject.to_s
 end
 
-
-#Get reviewable hits, and force expire early if consensus has been reached
-def earlyExpire
-	reviewable = @mturk.getReviewableHITs()
-end
-
 #get all hits and their status
-def getHITs
+#@param status: Filter by HITs with the given status. Options are 'Assignable', 'Unassignable', 'Reviewable', 'Reviewing', and 'Disposed'
+def getHITs(status="")
 	hits = @mturk.searchHITsAll.collect{|hit| hit[:HITId] }
-	result = Array.new
+	result = Hash.new
 	hits.each do |id|
-		puts @mturk.getHIT(HITId => id)
+		hit = @mturk.getHIT(:HITId => id)
+		if(status=="" || hit[:HITStatus]==status)
+			result[id] = hit
+		end
 	end
+	puts result
 end
 
-def createNewHIT(question, imageURL)
+#Get HIT answers
+#@param status: The status of the answers to recieve. Options are 'Submitted', 'Approved', and 'Rejected'
+def getAnswers(hit, status="")
+	answers = @mturk.getAssignmentsForHIT(:HITId => hit)
+	result = Hash.new
+	if(answers[:NumResults] == 1)
+		if(status=="" || answers[:AssignmentStatus]==status)
+			result[answers[:AssignmentId]] = answers
+		end
+	elsif(answers[:NumResults] > 1)
+		answers[:Assignment].each do |answer|
+			if(status=="" || answer[:AssignmentStatus]==status)
+				result[answer[:AssignmentId]] = answer
+			end
+		end
+	end
+	return result
+end
+
+def createNewHIT(questionId, imageId, imageURL)
 	title = "Food Classification"
-	desc = "The purpose of this task is to determine the types of food contained within the given image"
+	desc = "The purpose of this task is to determine the types of food contained within the given image."
 	keywords = "food, classification"
-	numAssignments = 5
-	rewardAmount = 0.05 # 5 cents
+	numAssignments = 1
+	rewardAmount = 0.01
 
 	# Define the location of the externalized question (QuestionForm) file.
-	questionFile = @base_directory+"questions/food_#{question}.question"
+	questionFile = @base_directory+"questions/food_#{questionId}.question"
 
 	# Load the question (QuestionForm) file
-	question = File.read( questionFile )
+	question = File.read(questionFile)
 	question = question.gsub(/\$imageURL/, imageURL)
 
 	result = @mturk.createHIT( :Title => title,
@@ -100,10 +127,10 @@ def createNewHIT(question, imageURL)
 	:Question => question,
 	:Keywords => keywords )
 
-	@db.prepare("INSERT INTO hit (image_id, task_tier, hit_id) VALUES ('#{row[0]}', 'question', '#{hit[:HITId]}')").execute
+	@db.execute("INSERT INTO hit (image_id, task_tier, hit_id) VALUES ('#{imageId}', '#{questionId}', '#{result[:HITId]}')")
 
-	puts "Created HIT: #{result[:HITId]}"
-	puts "HIT Location: #{getHITUrl( result[:HITTypeId] )}"
+	#puts "Created HIT: #{result[:HITId]}"
+	puts "     HIT Location: #{getHITUrl( result[:HITTypeId] )}"
 
 	return result
 end
@@ -118,23 +145,68 @@ end
 
 #Determine which tasks need to be created, and create them
 def genTasks
-	#Tier 0 tasks
-	result = db.prepare("SELECT * FROM image WHERE id NOT IN(SELECT image_id FROM hit)").execute
+	puts "--Generating new HITs--"
+	###### Tier 0 tasks ##########
+	result = @db.prepare("SELECT id, url FROM image WHERE id NOT IN(SELECT image_id FROM hit)").execute
 
 	i = 0
 	result.each do |row|
 		i += 1
-		hit = createNewHIT('', row[1])
+		createNewHIT('', row[0], row[1])
 	end
-
-	puts ""
-	puts "Created #{i} hits"
+	
+	####### All other tasks #######
+	
+	#Check for consensus
+	hits = @db.execute("SELECT hit_id FROM hit WHERE complete!=1")
+	
+	hits.each do |hit|
+		hit = hit[0]
+		image = @db.get_first_row("SELECT id, url FROM image WHERE id=(SELECT image_id FROM hit WHERE hit_id='#{hit}')")
+		hitDetail = @mturk.getHIT(:HITId => hit)
+		answers = getAnswers(hit, "Approved")
+		consensus = Hash.new
+		answers.each do |key, a|
+			@mturk.simplifyAnswer(a[:Answer]).each do |key, row|
+				row.each do |actualAnswer|
+					if(consensus.has_key?(actualAnswer))
+						consensus[actualAnswer] += 1
+					else
+						consensus[actualAnswer] = 1
+					end
+				end
+			end
+		end
+		
+		#do we have a majority vote yet?
+		majority = false
+		consensus.each do |key, c|
+			if(consensus[key] > hitDetail[:MaxAssignments].to_f/2)
+				majority = true
+			end
+		end
+		
+		#Gen new hits if we have majority or if all questions have been answered
+		if(majority || answers.length == hitDetail[:MaxAssignments].to_i)
+			if(majority)#quit early if we already have a majority vote
+				@mturk.forceExpireHIT(:HITId => hit)
+			end
+			
+			#generate new hits
+			consensus.each do |key, c|
+				if(c > hitDetail[:MaxAssignments].to_i/2)
+					i+=1
+					createNewHIT(key, image[0], image[1])
+				end
+			end
+			@db.execute("UPDATE hit SET complete=1 WHERE hit_id='#{hit}'")
+		end
+		
+	end
+	
+	puts "     Created #{i} hits"
 end
 
 ################ Main ################
-#processReviewableHits
-#earlyExpire
+processReviewableHits
 genTasks
-
-#puts @mturk.searchHITsAll.collect{|hit| hit[:HITId] }.length
-#getHits
